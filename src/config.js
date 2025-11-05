dbca5d183ec108659473a8728e272915afbe742d
jurenroy <baddergenius@gmail.com>
2025-11-05 11:54:12 +0800

    damn

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
-export const API_BASE_URL = 'http://192.168.1.250:8011'; // Your API base URL
-export const WS_BASE_URL = 'ws://192.168.1.250:8012/ws/chat/'; // Your API base URL
+export const API_BASE_URL = 'http://172.31.10.34:8011'; // Your API base URL
+export const WS_BASE_URL = 'ws://172.31.10.34:8012/ws/chat/'; // Your API base URL
 
